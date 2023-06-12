a9a698e5dfa8177d96c3578884394f73289eac34
subashini05 <hashinisuba555@gmail.com>
2023-06-12 22:57:07 +0530

    Initial commit

---
+++ New folder/expence_tracker/example/expence_tracker_example.dart
@@ -0,0 +1,6 @@
+import 'package:expence_tracker/expence_tracker.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ New folder/expence_tracker/test/expence_tracker_test.dart
@@ -0,0 +1,16 @@
+import 'package:expence_tracker/expence_tracker.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
